1216eb8b82e871f5c8e3e4dc3cb6ff6b2da5ea28
Xsy <1006937132@qq.com>
2023-02-03 17:21:55 +0800

    图片懒加载以及加载动画

--- src/application/Recommend/index.jsx
+++ src/application/Recommend/index.jsx
@@ -5,6 +5,8 @@ import Scroll from "../../baseUI/scroll/index";
 import { Content } from "./style";
 import { connect } from "react-redux";
 import * as actionTypes from "./store/actionCreators";
+import { forceCheck } from "react-lazyload";
+import Loading from "../../baseUI/loading";
 
 function Recommend(props) {
   // //mock 数据
@@ -26,13 +28,18 @@ function Recommend(props) {
   //   };
   // });
 
-  const { bannerList, recommendList } = props;
+  const { bannerList, recommendList, enterLoading } = props;
   const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
 
   useEffect(() => {
-    getBannerDataDispatch();
-    getRecommendListDataDispatch();
-    //eslint-disable-next-line
+    // 如果页面有数据，则不发请求
+    //immutable 数据结构中长度属性 size
+    if (!bannerList.size) {
+      getBannerDataDispatch();
+    }
+    if (!recommendList.size) {
+      getRecommendListDataDispatch();
+    }
   }, []);
 
   const bannerListJS = bannerList ? bannerList.toJS() : [];
@@ -40,12 +47,13 @@ function Recommend(props) {
 
   return (
     <Content>
-      <Scroll className="list">
+      <Scroll className="list" onScroll={forceCheck}>
         <div>
           <Slider bannerList={bannerListJS}></Slider>
           <RecommendList recommendList={recommendListJS}></RecommendList>
         </div>
       </Scroll>
+      {enterLoading ? <Loading></Loading> : null}
     </Content>
   );
 }
@@ -56,6 +64,7 @@ const mapStateToProps = (state) => ({
   // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
   bannerList: state.getIn(["recommend", "bannerList"]),
   recommendList: state.getIn(["recommend", "recommendList"]),
+  enterLoading: state.getIn(["recommend", "enterLoading"]),
 });
 // 映射 dispatch 到 props 上
 const mapDispatchToProps = (dispatch) => {
--- src/application/Recommend/store/reducer.js
+++ src/application/Recommend/store/reducer.js
@@ -4,6 +4,7 @@ import * as actionTypes from "./constants";
 import { fromJS } from "immutable"; // 这里用到 fromJS 把 JS 数据结构转化成 immutable 数据结构
 
 const defaultState = fromJS({
+  enterLoading: true,
   bannerList: [],
   recommendList: [],
 });
@@ -14,6 +15,8 @@ export default (state = defaultState, action) => {
       return state.set("bannerList", action.data);
     case actionTypes.CHANGE_RECOMMEND_LIST:
       return state.set("recommendList", action.data);
+    case actionTypes.CHANGE_ENTER_LOADING:
+      return state.set("enterLoading", action.data);
     default:
       return state;
   }
