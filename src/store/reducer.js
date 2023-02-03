eef6cd1dc7f19a4a1369478a3b5624a38be80fc4
Xsy <1006937132@qq.com>
2023-02-03 10:35:57 +0800

    接口联调

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,4 +1,6 @@
 import { combineReducers } from "redux-immutable";
+import { reducer as recommendReducer } from "../application/Recommend/store/index";
 export default combineReducers({
   // 之后开发具体功能模块时添加reducer
+  recommend: recommendReducer,
 });
